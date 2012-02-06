f923ca02ea80a95caa6a7e03c195ff20a2b51166
bfiss <bruno_fiss@yahoo.com.br>
2012-02-06 14:01:08 -0200

    changed analyzeTests to use log10

--- depth_complexity/analyzeTests.sh
+++ depth_complexity/analyzeTests.sh
@@ -30,7 +30,7 @@ do
     fi
     echo "set title \"${DIR}_${TERM[t]} Histogram\"" >> ${TMP}
     echo "set xlabel \"Depth Complexity\" " >> ${TMP}
-    echo "set ylabel \"Frequency (in log scale)\"" >> ${TMP}
+    echo "set ylabel \"Frequency (in log_10 scale)\"" >> ${TMP}
     DIV_CAR="plot"
     COUNT=0
     MAX=$( ls Tests/${DIR}/${TYPE[t]} | sort -nr | head -n 1 )
@@ -55,7 +55,7 @@ do
         done
         COR="#${VALUE[1]}${VALUE[2]}${VALUE[3]}"
         #echo "${COR}"
-        echo -n "${DIV_CAR} \"Tests/${DIR}/${TYPE[t]}/$d/hist.txt\" using 1:(log(\$2+1)+1) every ::1 w lines lt rgb \"${COR}\" title '$d'" >> ${TMP}
+        echo -n "${DIV_CAR} \"Tests/${DIR}/${TYPE[t]}/$d/hist.txt\" using 1:(log10(\$2+1)+1) every ::1 w lines lt rgb \"${COR}\" title '$d'" >> ${TMP}
 	    DIV_CAR=","
     done
     echo "" >> ${TMP}
